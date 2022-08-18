e82477bdaf0ecda9c5bbc9ea13b385eafaaad603
WangZhisong <2659460681@qq.com>
2022-08-18 13:10:04 +0800

    weapon、sensor新增字段，liquibase新增相关字段

--- modules/pooling-data-api/src/main/java/com/soul/pooling/entity/Sensor.java
+++ modules/pooling-data-api/src/main/java/com/soul/pooling/entity/Sensor.java
@@ -72,4 +72,170 @@ public class Sensor extends BaseEntity {
     @Column(name = "sensorCode")
     private String sensorCode;
 
+    //陆
+    @Display("最大作用距离_陆")
+    @Column(name = "maxDetectLandRange")
+    private String maxDetectLandRange;
+
+    @Display("最大作用高度_陆")
+    @Column(name = "maxDetectLandHeight")
+    private String maxDetectLandHeight;
+
+    @Display("最小作用高度_陆")
+    @Column(name = "minDetectLandHeight")
+    private String minDetectLandHeight;
+
+    @Display("最小目标特性_陆")
+    @Column(name = "minTargetLandFeature")
+    private String minTargetLandFeature;
+
+    @Display("情报周期_陆")
+    @Column(name = "landIntelligenceCycle")
+    private String landIntelligenceCycle;
+
+    @Display("处理目标数_陆")
+    @Column(name = "landHandleTargetNum")
+    private String landHandleTargetNum;
+
+    @Display("方位角_陆")
+    @Column(name = "landAzimuth")
+    private String landAzimuth;
+
+    @Display("精度_陆")
+    @Column(name = "landPrecision")
+    private String landPrecision;
+
+
+    //海
+    @Display("最大作用距离_海")
+    @Column(name = "maxDetectSeaRange")
+    private String maxDetectSeaRange;
+
+    @Display("最大作用高度_海")
+    @Column(name = "maxDetectSeaHeight")
+    private String maxDetectSeaHeight;
+
+    @Display("最小作用高度_海")
+    @Column(name = "minDetectSeaHeight")
+    private String minDetectSeaHeight;
+
+    @Display("最小目标特性_海")
+    @Column(name = "minSeaTargetFeature")
+    private String minSeaTargetFeature;
+
+    @Display("情报周期_海")
+    @Column(name = "seaIntelligenceCycle")
+    private String seaIntelligenceCycle;
+
+    @Display("处理目标数_海")
+    @Column(name = "seaHandleTargetNum")
+    private String seaHandleTargetNum;
+
+    @Display("方位角_海")
+    @Column(name = "seaAzimuth")
+    private String seaAzimuth;
+
+    @Display("精度_海")
+    @Column(name = "seaPrecision")
+    private String seaPrecision;
+
+    //空
+    @Display("最大作用距离_空")
+    @Column(name = "maxDetectAirRange")
+    private String maxDetectAirRange;
+
+    @Display("最大作用高度_空")
+    @Column(name = "maxDetectAirHeight")
+    private String maxDetectAirHeight;
+
+    @Display("最小作用高度_空")
+    @Column(name = "minDetectAirHeight")
+    private String minDetectAirHeight;
+
+    @Display("最小目标特性_空")
+    @Column(name = "minAirTargetFeature")
+    private String minAirTargetFeature;
+
+    @Display("情报周期_空")
+    @Column(name = "AirIntelligenceCycle")
+    private String AirIntelligenceCycle;
+
+    @Display("处理目标数_空")
+    @Column(name = "airHandleTargetNum")
+    private String airHandleTargetNum;
+
+    @Display("方位角_空")
+    @Column(name = "airAzimuth")
+    private String airAzimuth;
+
+    @Display("精度_空")
+    @Column(name = "airPrecision")
+    private String airPrecision;
+
+    //天
+    @Display("最大作用距离_天")
+    @Column(name = "maxDetectSpaceRange")
+    private String maxDetectSpaceRange;
+
+    @Display("最大作用高度_天")
+    @Column(name = "maxDetectSpaceHeight")
+    private String maxDetectSpaceHeight;
+
+    @Display("最小作用高度_天")
+    @Column(name = "minDetectSpaceHeight")
+    private String minDetectSpaceHeight;
+
+    @Display("最小目标特性_天")
+    @Column(name = "minSpaceTargetFeature")
+    private String minSpaceTargetFeature;
+
+    @Display("情报周期_天")
+    @Column(name = "spaceIntelligenceCycle")
+    private String spaceIntelligenceCycle;
+
+    @Display("处理目标数_天")
+    @Column(name = "spaceHandleTargetNum")
+    private String spaceHandleTargetNum;
+
+    @Display("方位角_天")
+    @Column(name = "spaceAzimuth")
+    private String spaceAzimuth;
+
+    @Display("精度_天")
+    @Column(name = "spacePrecision")
+    private String spacePrecision;
+
+    //潜
+    @Display("最大作用距离_潜")
+    @Column(name = "maxDetectUnderSeaRange")
+    private String maxDetectUnderSeaRange;
+
+    @Display("最大作用深度_潜")
+    @Column(name = "maxDetectUnderSeaDepth")
+    private String maxDetectUnderSeaDepth;
+
+    @Display("最小作用高度_潜")
+    @Column(name = "minDetectUnderSeaDepth")
+    private String minDetectUnderSeaDepth;
+
+    @Display("最小目标特性_潜")
+    @Column(name = "minUnderSeaTargetFeature")
+    private String minUnderSeaTargetFeature;
+
+    @Display("情报周期_潜")
+    @Column(name = "underSeaIntelligenceCycle")
+    private String underSeaIntelligenceCycle;
+
+    @Display("处理目标数_潜")
+    @Column(name = "underSeaHandleTargetNum")
+    private String underSeaHandleTargetNum;
+
+    @Display("方位角_潜")
+    @Column(name = "underSeaAzimuth")
+    private String underSeaAzimuth;
+
+    @Display("精度_潜")
+    @Column(name = "underSeaPrecision")
+    private String underSeaPrecision;
+
 }
--- modules/pooling-data-api/src/main/java/com/soul/pooling/entity/Weapon.java
+++ modules/pooling-data-api/src/main/java/com/soul/pooling/entity/Weapon.java
@@ -80,4 +80,124 @@ public class Weapon extends BaseEntity {
     @Column(name = "weaponCode")
     private String weaponCode;
 
+    //2022_08_18新增字段
+
+    @Display("制导方式")
+    @Column(name = "guidanceType")
+    private String guidanceType;
+
+    @Display("系统反应时间")
+    @Column(name = "reactionTime")
+    private String reactionTime;
+
+    @Display("最大飞行速度")
+    @Column(name = "maxSpeed")
+    private String maxSpeed;
+
+    @Display("齐射最小时间")
+    @Column(name = "salvoMinTime")
+    private String salvoMinTime;
+
+
+    //陆
+    @Display("最大射程_陆")
+    @Column(name = "maxFireLandRange")
+    private String maxFireLandRange;
+
+    @Display("最小射程_陆")
+    @Column(name = "minFireLandRange")
+    private String minFireLandRange;
+
+    @Display("最大射高_陆")
+    @Column(name = "maxFireLandHeight")
+    private String maxFireLandHeight;
+
+    @Display("最小射高_陆")
+    @Column(name = "minFireLandHeight")
+    private String minFireLandHeight;
+
+    @Display("命中概率_陆")
+    @Column(name = "landHitRate")
+    private float landHitRate;
+    //海
+    @Display("最大射程_海")
+    @Column(name = "maxFireSeaRange")
+    private String maxFireSeaRange;
+
+    @Display("最小射程_海")
+    @Column(name = "minFireSeaRange")
+    private String minFireSeaRange;
+
+    @Display("最大射高_海")
+    @Column(name = "maxFireSeaHeight")
+    private String maxFireSeaHeight;
+
+    @Display("最小射高_海")
+    @Column(name = "minFireSeaHeight")
+    private String minFireSeaHeight;
+
+    @Display("命中概率_海")
+    @Column(name = "seaHitRate")
+    private float seaHitRate;
+    //空
+    @Display("最大射程_空")
+    @Column(name = "maxFireAirRange")
+    private String maxFireAirRange;
+
+    @Display("最小射程_空")
+    @Column(name = "minFireAirRange")
+    private String minFireAirRange;
+
+    @Display("最大射高_空")
+    @Column(name = "maxFireAirHeight")
+    private String maxFireAirHeight;
+
+    @Display("最小射高_空")
+    @Column(name = "minFireAirHeight")
+    private String minFireAirHeight;
+
+    @Display("命中概率_空")
+    @Column(name = "airHitRate")
+    private float airHitRate;
+    //天
+    @Display("最大射程_天")
+    @Column(name = "maxFireSpaceRange")
+    private String maxFireSpaceRange;
+
+    @Display("最小射程_天")
+    @Column(name = "minFireSpaceRange")
+    private String minFireSpaceRange;
+
+    @Display("最大射高_天")
+    @Column(name = "maxFireSpaceHeight")
+    private String maxFireSpaceHeight;
+
+    @Display("最小射高_天")
+    @Column(name = "minFireSpaceHeight")
+    private String minFireSpaceHeight;
+
+    @Display("命中概率_天")
+    @Column(name = "spaceHitRate")
+    private float spaceHitRate;
+    //潜
+    @Display("最大射程_潜")
+    @Column(name = "maxFireUnderSeaRange")
+    private String maxFireUnderSeaRange;
+
+    @Display("最小射程_潜")
+    @Column(name = "minFireUnderSeaRange")
+    private String minFireUnderSeaRange;
+
+    @Display("最大深度_潜")
+    @Column(name = "maxFireUnderSeaDepth")
+    private String maxFireUnderSeaDepth;
+
+    @Display("最小深度_潜")
+    @Column(name = "minFireUnderSeaDepth")
+    private String minFireUnderSeaDepth;
+
+    @Display("命中概率_潜")
+    @Column(name = "underSeaHitRate")
+    private float underSeaHitRate;
+
 }
